0745f0d4b228a93fb3c1b0999fd931cc71db747c
sobrepenaej@yahoo.com <sobrepenaej@yahoo.com>
2023-09-23 13:22:59 +0300

    Flying-Ducks Added the fly() method

--- src/main/java/org/hfdp/tla/javastarter/App.java
+++ src/main/java/org/hfdp/tla/javastarter/App.java
@@ -12,11 +12,13 @@ public class App
         MallardDuck mallard = new MallardDuck();
         mallard.swim();
         mallard.quack();
+        mallard.fly();
         mallard.display();
 
         RedheadDuck redhead = new RedheadDuck();
         redhead.swim();
         redhead.quack();
+        redhead.fly();
         redhead.display();
 
     }
--- src/main/java/org/hfdp/tla/javastarter/Duck.java
+++ src/main/java/org/hfdp/tla/javastarter/Duck.java
@@ -10,5 +10,9 @@ public abstract class Duck {
         System.out.println("Quack");
     }
 
+    public void fly() {
+        System.out.println("Fly");
+    }
+
     public abstract void display();
 }
